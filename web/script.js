940821e6de429cdf10f333043585d64e41a94696
kevdinta <kevin.dintanavong@hotmail.fr>
2017-09-30 17:44:23 +0200

    Navbar v2 Changement de couleur en progression Remplacement du slider par un parallax incoming

---
+++ web/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by DINTANAVONG on 30/09/2017.
+ */
+
+// Display carousel
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+// Initialize collapse button
+$(".se_connecter").sideNav();
